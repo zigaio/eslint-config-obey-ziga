be582edaf5f70a107c7626e27f23cdc9f423e63c
Daniel Ziga <danielziga@outlook.com>
2021-03-21 23:22:39 +0100

    feat: Add first batch of react rules.

--- rules/react.js
+++ rules/react.js
@@ -1,4 +1,16 @@
 module.exports = {
   plugins: ['react'],
-  rules: {},
+  rules: {
+    'react/boolean-prop-naming': [2, {
+      rule: '^(is|has)[A-Z]([A-Za-z0-9]?)+(ed)$',
+      message: 'Boolean props must be prefixed with "is" or "has"',
+    }],
+    'react/destructuring-assignment': [2, 'always'],
+    'react/forbid-component-props': 0,
+    'react/forbid-dom-props': 0,
+    'react/forbid-elements': 0,
+    'react/forbid-foreign-prop-types': 2,
+    'react/forbid-prop-types': 2,
+    'react/function-component-definition': 2,
+  },
 };
