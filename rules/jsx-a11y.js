2e8d94dc295ba373e217e5478a9c8bd8a12d767b
Daniel Ziga <danielziga@outlook.com>
2021-03-22 15:46:22 +0100

    refactor: Enable html-has-lang again.

--- rules/jsx-a11y.js
+++ rules/jsx-a11y.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: ['jsx-a11y'],
   rules: {
     'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/html-has-lang': 0,
     'jsx-a11y/mouse-events-have-key-events': 0,
     'jsx-a11y/no-access-key': 0,
     'jsx-a11y/no-autofocus': 0,
