625c6666dec7344977a5de98e4f04573bb258f3a
Daniel Ziga <danielziga@outlook.com>
2021-05-06 01:15:27 +0200

    Ignore common numbers for no-magic-numbers.

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -10,6 +10,13 @@ module.exports = {
     }],
     'no-floating-decimal': 0,
     'no-magic-numbers': [2, {
+      ignore: [
+        0, // Common starting number.
+        1, // Common Array.length comparor.
+        60, // Usually used for calculating time.
+        1000, // Usually used for converting time to ms.
+        1024 // Usually used for calculating byte sizes.
+      ],
       ignoreArrayIndexes: true,
       detectObjects: true,
       enforceConst: true,
